d4efc6774392f3eba0ba321cb4b084056ae3cf5e
Nahid1911 <nahid.raihan@gmail.com>
2023-11-10 15:19:06 +0600

    Install Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "devise/sessions#new"
+  devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
